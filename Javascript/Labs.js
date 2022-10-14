39bb774a6b23afa97f0be9f31f0f3837c775f8ad
JWhit-afk <69322810+JWhit-afk@users.noreply.github.com>
2022-10-14 16:19:40 +0100

    Lab 6 progress

--- Javascript/Labs.js
+++ Javascript/Labs.js
@@ -1,4 +1,21 @@
+let textLarge = false;
+
 function eventHandler() {
+    if (textLarge) {
+        makeSmaller();
+        textLarge = false;
+    } else {
+        makeLarger();
+        textLarge = true;
+    }
+}
+
+function makeLarger() {
     document.getElementById("Lab1").style.color = "blue";
     document.getElementById("Lab1").style.fontSize = "100px";
+}
+
+function makeSmaller() {
+    document.getElementById("Lab1").style.color = "black";
+    document.getElementById("Lab1").style.fontSize = "16px";
 }
